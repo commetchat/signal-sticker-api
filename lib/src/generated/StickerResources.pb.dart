2d864d0f907d698f260e63f44b3aacc400f089ac
Airyzz <36567925+Airyzz@users.noreply.github.com>
2026-05-23 09:34:04 +0930

    update protobuf

--- lib/src/generated/StickerResources.pb.dart
+++ lib/src/generated/StickerResources.pb.dart
@@ -1,157 +1,175 @@
-///
-//  Generated code. Do not modify.
-//  source: StickerResources.proto
+// This is a generated file - do not edit.
 //
-// @dart = 2.12
-// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
+// Generated from src/protobuf/StickerResources.proto.
+
+// @dart = 3.3
+
+// ignore_for_file: annotate_overrides, camel_case_types, comment_references
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: curly_braces_in_flow_control_structures
+// ignore_for_file: deprecated_member_use_from_same_package, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_relative_imports
 
 import 'dart:core' as $core;
 
 import 'package:protobuf/protobuf.dart' as $pb;
 
-class Pack_Sticker extends $pb.GeneratedMessage {
-  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Pack.Sticker', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'signalservice'), createEmptyInstance: create)
-    ..a<$core.int>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'id', $pb.PbFieldType.OU3)
-    ..aOS(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'emoji')
-    ..hasRequiredFields = false
-  ;
+export 'package:protobuf/protobuf.dart' show GeneratedMessageGenericExtensions;
 
-  Pack_Sticker._() : super();
+class Pack_Sticker extends $pb.GeneratedMessage {
   factory Pack_Sticker({
     $core.int? id,
     $core.String? emoji,
   }) {
-    final _result = create();
-    if (id != null) {
-      _result.id = id;
-    }
-    if (emoji != null) {
-      _result.emoji = emoji;
-    }
-    return _result;
+    final result = create();
+    if (id != null) result.id = id;
+    if (emoji != null) result.emoji = emoji;
+    return result;
   }
-  factory Pack_Sticker.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
-  factory Pack_Sticker.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
-  @$core.Deprecated(
-  'Using this can add significant overhead to your binary. '
-  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
-  'Will be removed in next major version')
-  Pack_Sticker clone() => Pack_Sticker()..mergeFromMessage(this);
-  @$core.Deprecated(
-  'Using this can add significant overhead to your binary. '
-  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
-  'Will be removed in next major version')
-  Pack_Sticker copyWith(void Function(Pack_Sticker) updates) => super.copyWith((message) => updates(message as Pack_Sticker)) as Pack_Sticker; // ignore: deprecated_member_use
+
+  Pack_Sticker._();
+
+  factory Pack_Sticker.fromBuffer($core.List<$core.int> data,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromBuffer(data, registry);
+  factory Pack_Sticker.fromJson($core.String json,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromJson(json, registry);
+
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
+      _omitMessageNames ? '' : 'Pack.Sticker',
+      package: const $pb.PackageName(_omitMessageNames ? '' : 'signalservice'),
+      createEmptyInstance: create)
+    ..aI(1, _omitFieldNames ? '' : 'id', fieldType: $pb.PbFieldType.OU3)
+    ..aOS(2, _omitFieldNames ? '' : 'emoji')
+    ..hasRequiredFields = false;
+
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
+  Pack_Sticker clone() => deepCopy();
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
+  Pack_Sticker copyWith(void Function(Pack_Sticker) updates) =>
+      super.copyWith((message) => updates(message as Pack_Sticker))
+          as Pack_Sticker;
+
+  @$core.override
   $pb.BuilderInfo get info_ => _i;
+
   @$core.pragma('dart2js:noInline')
   static Pack_Sticker create() => Pack_Sticker._();
+  @$core.override
   Pack_Sticker createEmptyInstance() => create();
-  static $pb.PbList<Pack_Sticker> createRepeated() => $pb.PbList<Pack_Sticker>();
   @$core.pragma('dart2js:noInline')
-  static Pack_Sticker getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Pack_Sticker>(create);
+  static Pack_Sticker getDefault() => _defaultInstance ??=
+      $pb.GeneratedMessage.$_defaultFor<Pack_Sticker>(create);
   static Pack_Sticker? _defaultInstance;
 
   @$pb.TagNumber(1)
   $core.int get id => $_getIZ(0);
   @$pb.TagNumber(1)
-  set id($core.int v) { $_setUnsignedInt32(0, v); }
+  set id($core.int value) => $_setUnsignedInt32(0, value);
   @$pb.TagNumber(1)
   $core.bool hasId() => $_has(0);
   @$pb.TagNumber(1)
-  void clearId() => clearField(1);
+  void clearId() => $_clearField(1);
 
   @$pb.TagNumber(2)
   $core.String get emoji => $_getSZ(1);
   @$pb.TagNumber(2)
-  set emoji($core.String v) { $_setString(1, v); }
+  set emoji($core.String value) => $_setString(1, value);
   @$pb.TagNumber(2)
   $core.bool hasEmoji() => $_has(1);
   @$pb.TagNumber(2)
-  void clearEmoji() => clearField(2);
+  void clearEmoji() => $_clearField(2);
 }
 
 class Pack extends $pb.GeneratedMessage {
-  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Pack', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'signalservice'), createEmptyInstance: create)
-    ..aOS(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'title')
-    ..aOS(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'author')
-    ..aOM<Pack_Sticker>(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'cover', subBuilder: Pack_Sticker.create)
-    ..pc<Pack_Sticker>(4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'stickers', $pb.PbFieldType.PM, subBuilder: Pack_Sticker.create)
-    ..hasRequiredFields = false
-  ;
-
-  Pack._() : super();
   factory Pack({
     $core.String? title,
     $core.String? author,
     Pack_Sticker? cover,
     $core.Iterable<Pack_Sticker>? stickers,
   }) {
-    final _result = create();
-    if (title != null) {
-      _result.title = title;
-    }
-    if (author != null) {
-      _result.author = author;
-    }
-    if (cover != null) {
-      _result.cover = cover;
-    }
-    if (stickers != null) {
-      _result.stickers.addAll(stickers);
-    }
-    return _result;
+    final result = create();
+    if (title != null) result.title = title;
+    if (author != null) result.author = author;
+    if (cover != null) result.cover = cover;
+    if (stickers != null) result.stickers.addAll(stickers);
+    return result;
   }
-  factory Pack.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
-  factory Pack.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
-  @$core.Deprecated(
-  'Using this can add significant overhead to your binary. '
-  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
-  'Will be removed in next major version')
-  Pack clone() => Pack()..mergeFromMessage(this);
-  @$core.Deprecated(
-  'Using this can add significant overhead to your binary. '
-  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
-  'Will be removed in next major version')
-  Pack copyWith(void Function(Pack) updates) => super.copyWith((message) => updates(message as Pack)) as Pack; // ignore: deprecated_member_use
+
+  Pack._();
+
+  factory Pack.fromBuffer($core.List<$core.int> data,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromBuffer(data, registry);
+  factory Pack.fromJson($core.String json,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromJson(json, registry);
+
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
+      _omitMessageNames ? '' : 'Pack',
+      package: const $pb.PackageName(_omitMessageNames ? '' : 'signalservice'),
+      createEmptyInstance: create)
+    ..aOS(1, _omitFieldNames ? '' : 'title')
+    ..aOS(2, _omitFieldNames ? '' : 'author')
+    ..aOM<Pack_Sticker>(3, _omitFieldNames ? '' : 'cover',
+        subBuilder: Pack_Sticker.create)
+    ..pPM<Pack_Sticker>(4, _omitFieldNames ? '' : 'stickers',
+        subBuilder: Pack_Sticker.create)
+    ..hasRequiredFields = false;
+
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
+  Pack clone() => deepCopy();
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
+  Pack copyWith(void Function(Pack) updates) =>
+      super.copyWith((message) => updates(message as Pack)) as Pack;
+
+  @$core.override
   $pb.BuilderInfo get info_ => _i;
+
   @$core.pragma('dart2js:noInline')
   static Pack create() => Pack._();
+  @$core.override
   Pack createEmptyInstance() => create();
-  static $pb.PbList<Pack> createRepeated() => $pb.PbList<Pack>();
   @$core.pragma('dart2js:noInline')
-  static Pack getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Pack>(create);
+  static Pack getDefault() =>
+      _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Pack>(create);
   static Pack? _defaultInstance;
 
   @$pb.TagNumber(1)
   $core.String get title => $_getSZ(0);
   @$pb.TagNumber(1)
-  set title($core.String v) { $_setString(0, v); }
+  set title($core.String value) => $_setString(0, value);
   @$pb.TagNumber(1)
   $core.bool hasTitle() => $_has(0);
   @$pb.TagNumber(1)
-  void clearTitle() => clearField(1);
+  void clearTitle() => $_clearField(1);
 
   @$pb.TagNumber(2)
   $core.String get author => $_getSZ(1);
   @$pb.TagNumber(2)
-  set author($core.String v) { $_setString(1, v); }
+  set author($core.String value) => $_setString(1, value);
   @$pb.TagNumber(2)
   $core.bool hasAuthor() => $_has(1);
   @$pb.TagNumber(2)
-  void clearAuthor() => clearField(2);
+  void clearAuthor() => $_clearField(2);
 
   @$pb.TagNumber(3)
   Pack_Sticker get cover => $_getN(2);
   @$pb.TagNumber(3)
-  set cover(Pack_Sticker v) { setField(3, v); }
+  set cover(Pack_Sticker value) => $_setField(3, value);
   @$pb.TagNumber(3)
   $core.bool hasCover() => $_has(2);
   @$pb.TagNumber(3)
-  void clearCover() => clearField(3);
+  void clearCover() => $_clearField(3);
   @$pb.TagNumber(3)
   Pack_Sticker ensureCover() => $_ensure(2);
 
   @$pb.TagNumber(4)
-  $core.List<Pack_Sticker> get stickers => $_getList(3);
+  $pb.PbList<Pack_Sticker> get stickers => $_getList(3);
 }
 
+const $core.bool _omitFieldNames =
+    $core.bool.fromEnvironment('protobuf.omit_field_names');
+const $core.bool _omitMessageNames =
+    $core.bool.fromEnvironment('protobuf.omit_message_names');
--- lib/src/generated/StickerResources.pbjson.dart
+++ lib/src/generated/StickerResources.pbjson.dart
@@ -1,33 +1,58 @@
-///
-//  Generated code. Do not modify.
-//  source: StickerResources.proto
+// This is a generated file - do not edit.
 //
-// @dart = 2.12
-// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,deprecated_member_use_from_same_package,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
+// Generated from src/protobuf/StickerResources.proto.
 
-import 'dart:core' as $core;
+// @dart = 3.3
+
+// ignore_for_file: annotate_overrides, camel_case_types, comment_references
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: curly_braces_in_flow_control_structures
+// ignore_for_file: deprecated_member_use_from_same_package, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_relative_imports
+// ignore_for_file: unused_import
+
 import 'dart:convert' as $convert;
+import 'dart:core' as $core;
 import 'dart:typed_data' as $typed_data;
+
 @$core.Deprecated('Use packDescriptor instead')
-const Pack$json = const {
+const Pack$json = {
   '1': 'Pack',
-  '2': const [
-    const {'1': 'title', '3': 1, '4': 1, '5': 9, '10': 'title'},
-    const {'1': 'author', '3': 2, '4': 1, '5': 9, '10': 'author'},
-    const {'1': 'cover', '3': 3, '4': 1, '5': 11, '6': '.signalservice.Pack.Sticker', '10': 'cover'},
-    const {'1': 'stickers', '3': 4, '4': 3, '5': 11, '6': '.signalservice.Pack.Sticker', '10': 'stickers'},
+  '2': [
+    {'1': 'title', '3': 1, '4': 1, '5': 9, '10': 'title'},
+    {'1': 'author', '3': 2, '4': 1, '5': 9, '10': 'author'},
+    {
+      '1': 'cover',
+      '3': 3,
+      '4': 1,
+      '5': 11,
+      '6': '.signalservice.Pack.Sticker',
+      '10': 'cover'
+    },
+    {
+      '1': 'stickers',
+      '3': 4,
+      '4': 3,
+      '5': 11,
+      '6': '.signalservice.Pack.Sticker',
+      '10': 'stickers'
+    },
   ],
-  '3': const [Pack_Sticker$json],
+  '3': [Pack_Sticker$json],
 };
 
 @$core.Deprecated('Use packDescriptor instead')
-const Pack_Sticker$json = const {
+const Pack_Sticker$json = {
   '1': 'Sticker',
-  '2': const [
-    const {'1': 'id', '3': 1, '4': 1, '5': 13, '10': 'id'},
-    const {'1': 'emoji', '3': 2, '4': 1, '5': 9, '10': 'emoji'},
+  '2': [
+    {'1': 'id', '3': 1, '4': 1, '5': 13, '10': 'id'},
+    {'1': 'emoji', '3': 2, '4': 1, '5': 9, '10': 'emoji'},
   ],
 };
 
 /// Descriptor for `Pack`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List packDescriptor = $convert.base64Decode('CgRQYWNrEhQKBXRpdGxlGAEgASgJUgV0aXRsZRIWCgZhdXRob3IYAiABKAlSBmF1dGhvchIxCgVjb3ZlchgDIAEoCzIbLnNpZ25hbHNlcnZpY2UuUGFjay5TdGlja2VyUgVjb3ZlchI3CghzdGlja2VycxgEIAMoCzIbLnNpZ25hbHNlcnZpY2UuUGFjay5TdGlja2VyUghzdGlja2VycxovCgdTdGlja2VyEg4KAmlkGAEgASgNUgJpZBIUCgVlbW9qaRgCIAEoCVIFZW1vamk=');
+final $typed_data.Uint8List packDescriptor = $convert.base64Decode(
+    'CgRQYWNrEhQKBXRpdGxlGAEgASgJUgV0aXRsZRIWCgZhdXRob3IYAiABKAlSBmF1dGhvchIxCg'
+    'Vjb3ZlchgDIAEoCzIbLnNpZ25hbHNlcnZpY2UuUGFjay5TdGlja2VyUgVjb3ZlchI3CghzdGlj'
+    'a2VycxgEIAMoCzIbLnNpZ25hbHNlcnZpY2UuUGFjay5TdGlja2VyUghzdGlja2VycxovCgdTdG'
+    'lja2VyEg4KAmlkGAEgASgNUgJpZBIUCgVlbW9qaRgCIAEoCVIFZW1vamk=');
